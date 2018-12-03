53709318c8d1944094c4d2fea9a4e49a72d14934
Swapnil Bharshankar <bharshankar.swapnil@gmail.com>
2018-12-03 18:03:47 +0000

    ok

--- startApp.sh
+++ startApp.sh
@@ -4,6 +4,6 @@ export DB_PRD_HOST=postgresql.ck4upkzrdp3w.us-east-1.rds.amazonaws.com
 export DB_PRD_USER=postgresql
 export DB_PRD_PASS=postgresql123
 export NODE_HOST=18.213.113.157
-export NODE_PORT=8080
+export NODE_PORT=3000
 node /home/ubuntu/hapi-rest-demo/index.js&
 exit 0
